aedf2fd2ffe45fa215d2aa1aabe8f1beb4e3985b
Diana-nyamai <dnyamai.dn@gmail.com>
2021-02-07 20:40:51 +0300

    update

---
+++ react-portfolio/src/Components/AboutPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class AboutPage extends Component {
+    render() {
+        return (
+            <div>
+                This Is the about AboutPage
+            </div>
+        )
+    }
+}
+
+export default AboutPage
