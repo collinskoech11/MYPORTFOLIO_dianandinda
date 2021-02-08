f05bdb528bd6de3096a507bf37c5ba77c3a37f5f
Diana-nyamai <dnyamai.dn@gmail.com>
2021-02-08 21:23:50 +0300

    skills page updae

---
+++ react-portfolio/src/Components/Skills.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Skills extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is the Skills page</h1>
+            </div>
+        )
+    }
+}
+
+export default Skills
