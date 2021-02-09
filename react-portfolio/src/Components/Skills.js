965ddae68a63aa6f69e1b74b598bd7e7607a71b8
Diana-nyamai <dnyamai.dn@gmail.com>
2021-02-09 21:17:18 +0300

    update

--- react-portfolio/src/Components/Skills.js
+++ react-portfolio/src/Components/Skills.js
@@ -3,9 +3,33 @@ import React, { Component } from 'react'
 export class Skills extends Component {
     render() {
         return (
-            <div>
-                <h1>This is the Skills page</h1>
-            </div>
+           <div>
+           <section id="two">
+           <div class="row" style="padding: 30px;">
+   <article class="col-6 col-12-xsmall work-item">
+   <a href="images copy/certs/CSS_certificate.jpg" class="image fit thumb"><img src="images copy/certs/JavaScript_certificate.jpg" alt="" /></a>
+   <h3>JavaScript_certificate</h3>
+   </article>
+   <article class="col-6 col-12-xsmall work-item">
+   <a href="images copy/certs/PHP_certificate.jpg" class="image fit thumb"><img src="images copy/certs/PHP_certificate.jpg" alt="" /></a>
+   <h3>PHP_certificate</h3>
+   </article>
+   <article class="col-6 col-12-xsmall work-item">
+   <a href="images copy/certs/react_certificate.jpg" class="image fit thumb"><img src="images copy/certs/react_certificate.jpg" alt="" /></a>
+   <h3>react_certificate</h3>
+   </article>
+   <article class="col-6 col-12-xsmall work-item">
+   <a href="images copy/certs/HTML_certificate.jpg" class="image fit thumb"><img src="images copy/certs/HTML_certificate.jpg" alt="" /></a>
+   <h3>HTML_certificate</h3>
+   </article>
+   <article class="col-6 col-12-xsmall work-item">
+   <a href="images copy/certs/CSS_certificate.jpg" class="image fit thumb"><img src="images copy/certs/CSS_certificate.jpg" alt="" /></a>
+   <h3>CSS_certificate</h3>
+   </article>
+   </div>
+           
+       </section>
+           </div>
         )
     }
 }
