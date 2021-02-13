a212b84d72e467fa832b5cc764cc6412cb2e070b
collinskoech11 <collinskoechck34@gmail.com>
2021-02-13 18:46:48 +0300

    update

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 
-function App() {
+import { Gradient } from 'react-gradient';
+
+const gradients = [
+  ['#bd19d6', '#ea7d10'],
+  ['#ff2121', '#25c668'],
+];
+export default function App() {
   return (
     <div className="App">
-      
+       <Gradient
+                gradients={ gradients } // required
+                property="background"
+                duration={ 3000 }
+                angle="45deg"
+            >
+
+            </Gradient>
     </div>
   );
 }
 
-export default App;
+
