d7356f2348b220c1ea358a1f91000a4ac76ec12b
Diana-nyamai <dnyamai.dn@gmail.com>
2021-02-14 19:45:12 +0300

    myworkk update

--- react-portfolio/src/Components/MyWork.js
+++ react-portfolio/src/Components/MyWork.js
@@ -4,7 +4,29 @@ export class MyWork extends Component {
     render() {
         return (
             <div>
-              
+            <div class="row" style="padding: 30px;">
+            <article class="col-6 col-12-xsmall work-item">
+                <a href="images copy/mywork/mywork1.jpeg" class="image fit thumb"><img src="images copy/mywork/mywork1.jpeg" alt="" /></a>
+                <h3>Mochoice-website: An e-commerce site</h3>
+                <a href=""></a><button type="button" class="btn btn-secondary">view website</button>
+            </article>
+            <article class="col-6 col-12-xsmall work-item">
+                <a href="images copy/mywork/mywork4.jpeg" class="image fit thumb"><img src="images copy/mywork/mywork4.jpeg" alt="" /></a>
+                <h3>Fundis-website: A repair and fixtures site</h3>
+                <a href=""></a><button type="button" class="btn btn-secondary">view website</button>
+            </article>
+            <article class="col-6 col-12-xsmall work-item">
+                <a href="images copy/mywork/mywork2.jpg" class="image fit thumb"><img src="images copy/mywork/mywork2.jpg" alt="" /></a>
+                <h3>Gikomba.Online: A thift e-commerce site</h3>
+                <a href=""></a><button type="button" class="btn btn-secondary">view website</button>
+            </article>
+            <article class="col-6 col-12-xsmall work-item">
+                <a href="images copy/mywork/amie.jpeg" class="image fit thumb"><img src="images copy/mywork/amie.jpeg" alt="" /></a>
+                <h3>Amie Foundation: A charity organisation</h3>
+                <a href="https://github.com/Diana-nyamai/DscTwo"></a><button type="button" class="btn btn-secondary">view code</button>
+                <a href=""></a><button type="button" class="btn btn-secondary">view website</button>
+            </article>
+            </div>
             </div>
         )
     }
