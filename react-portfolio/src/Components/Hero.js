7d79c802a1938370746a2e7822902567b4dcd677
collinskoech11 <collinskoechck34@gmail.com>
2021-02-17 11:13:47 +0300

    update

--- react-portfolio/src/Components/Hero.js
+++ react-portfolio/src/Components/Hero.js
@@ -6,6 +6,7 @@ function Hero() {
             <h1>Diana Ndinda Nyamai</h1>
             <p>Hello, Welcome to my portfolio</p>
             <p>Feel free to contact me if you like what you see</p>
+            
         </div>
     )
 }
