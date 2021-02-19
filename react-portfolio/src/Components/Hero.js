b5233d376293fb4861a182d1459eeddff212609d
Diana-nyamai <dnyamai.dn@gmail.com>
2021-02-19 11:59:39 +0300

    update

--- react-portfolio/src/Components/Hero.js
+++ react-portfolio/src/Components/Hero.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Hero() {
     return (
-        <div className="content" style={{textAlign:'center',height:'100vh', paddingTop:'10em'}}>
+        <div className="content" style={{textAlign:'center', paddingTop:'10em'}}>
             <h1 style={{color:'#fff'}}>Diana Ndinda Nyamai</h1>
             <p style={{color:'#fff'}}>Hello, Welcome to my portfolio</p>
             <p style={{color:'#fff'}}>Feel free to contact me if you like what you see</p>
