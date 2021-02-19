8b2f57a84b166178623b3c046b76bfc872a78936
Diana-nyamai <dnyamai.dn@gmail.com>
2021-02-19 14:26:10 +0300

    update

--- react-portfolio/src/Components/Home.js
+++ react-portfolio/src/Components/Home.js
@@ -6,7 +6,11 @@ export class Home extends Component {
             <div>
                <div className="main" >
                <div className="nav">
-               
+               <ul>
+               <li>About Me </li>
+               <li>My Projects </li>
+               <li>My Skills </li>
+               </ul>
                </div>
                </div>
             </div>
