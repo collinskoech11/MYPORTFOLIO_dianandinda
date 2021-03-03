87d2a38e2d2d0d75b0bad0a1abd1ac2d1771967c
Diana-nyamai <dnyamai.dn@gmail.com>
2021-03-03 18:27:10 +0300

    update

--- react-portfolio/src/Components/Home.js
+++ react-portfolio/src/Components/Home.js
@@ -21,8 +21,8 @@ export class Home extends Component {
                <div className="row" style={{padding:'20px'}}>
                <div className="col-lg-4 col-md-2 h-100" style={{paddingTop:'50px'}}>
                My name is Diana Ndinda Nyamai.i am a dedicated and motivated software developer
-i am experienced in frontend developer and i have worked with the company Fundis as an intern.
-The languages and technologies i am good at include javascript,java,python,react. i love technology and always ready to learn something new.
+               i am experienced in frontend developer and i have worked with the company Fundis as an intern.
+               The languages and technologies i am good at include javascript,java,python,react. i love technology and always ready to learn something new.
                </div>
                <div className="col-lg-6 col-md-6" style={{paddingLeft:'200px'}}>
                <div className="profile">  <img className="img-fluid" src="./Images/profile.png" alt="profilepic"/></div>
