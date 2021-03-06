0005c3f321754393af4d45fb4cd62e0211b35ad8
Diana-nyamai <dnyamai.dn@gmail.com>
2021-03-06 15:13:01 +0300

    update

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -3,18 +3,13 @@ import './Css/Home.css';
 
 
 import Hero from './Components/Hero';
-import { Gradient } from 'react-gradient';
 import Home from './Components/Home';
 
 
-const gradients = [
-  ['#bd19d6', '#ea7d10'],
-  ['#ff2121', '#25c668'],
-];
 export default function App() {
   return (
      <div> 
-     <Hero/>
+             <Hero/>
               <Home/>
               
               </div>
