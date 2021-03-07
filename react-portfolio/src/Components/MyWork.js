caa9560424e8fbefca0057d085f18ffb87a512b4
Diana-nyamai <dnyamai.dn@gmail.com>
2021-03-07 22:54:47 +0300

    update

--- react-portfolio/src/Components/MyWork.js
+++ react-portfolio/src/Components/MyWork.js
@@ -10,11 +10,13 @@ export class MyWork extends Component {
                 <a href="images copy/mywork/mywork1.jpeg" class="image fit thumb"><img src="images copy/mywork/mywork1.jpeg" alt="" /></a>
                 <h3>Mochoice-website: An e-commerce site</h3>
            <button type="button" class="btn btn-secondary">view website</button>
+           <button type="button" class="btn btn-secondary">view code</button>
             </article>
             <article class="col-6 col-12-xsmall work-item">
                 <a href="images copy/mywork/mywork4.jpeg" class="image fit thumb"><img src="images copy/mywork/mywork4.jpeg" alt="" /></a>
                 <h3>Fundis-website: A repair and fixtures site</h3>
                 <button type="button" class="btn btn-secondary">view website</button>
+                <button type="button" class="btn btn-secondary">view code</button>
             </article>
             <article class="col-6 col-12-xsmall work-item">
                 <a href="images copy/mywork/mywork2.jpg" class="image fit thumb"><img src="images copy/mywork/mywork2.jpg" alt="" /></a>
