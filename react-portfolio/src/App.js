d036efb6cf0659e6e8f41392beca595d7d632396
Diana-nyamai <dnyamai.dn@gmail.com>
2021-03-12 18:38:47 +0300

    update

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -8,7 +8,7 @@ import Home from './Components/Home';
 
 export default function App() {
   return (
-     <div> 
+     <div className="App"> 
              <Hero/>
               <Home/>
               </div>
